e10ac3a93229c1d4dc0c08e8e2cb8afc84ad8785
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2024-02-21 21:05:16 -0300

    Cambio Slider

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 /* -----------------------------------------
   Have focus outline only for keyboard users 
  ---------------------------------------- */
-
+ const slides = document.querySelectorAll('.slide');
+ let currentSlide = 0;
+ 
+ function showSlide(n) {
+   slides[currentSlide].style.display = 'none';
+   currentSlide = (n + slides.length) % slides.length;
+   slides[currentSlide].style.display = 'block';
+ }
+ 
+ function nextSlide() {
+   showSlide(currentSlide + 1);
+ }
+ 
+ setInterval(nextSlide, 5000); // Cambia el slide cada 5 segundos
+ 
 const handleFirstTab = (e) => {
   if(e.key === 'Tab') {
     document.body.classList.add('user-is-tabbing')
